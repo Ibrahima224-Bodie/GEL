d2634f8ebe805070c212c6e1bd47764170a08919
Ibrahima224-Bodie <102962442+Ibrahima224-Bodie@users.noreply.github.com>
2022-06-01 18:10:25 +0000

    c'est le premier dépôt

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,101 @@
+<x-app-layout>
+    <x-slot name="header">
+    
+    </x-slot>
+
+    <div>
+        <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GEL</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+     <!-- MORRIS CHART STYLES-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+        </nav>   
+           <!-- /. NAV TOP  -->
+                <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				<li class="text-center">
+                    
+                    <li>
+                        <a  href="table.html"><i class="fa fa-table fa-3x"></i> LISTE DES AFFECTIONS</a>
+                    </li>
+                    <li>
+                        <a  href="table.html"><i class="fa fa-table fa-3x"></i> LISTE DU PERSONNEL</a>
+                    </li>
+                    <li>
+                        <a  href="table.html"><i class="fa fa-table fa-3x"></i> LISTE DES ENGINS</a>
+                    </li>
+                    <li>
+                        <a  href="table.html"><i class="fa fa-table fa-3x"></i> LISTE DES SERVICES</a>
+                    </li>
+                    <li>
+                        <a  href="table.html"><i class="fa fa-table fa-3x"></i> LISTE DES TYPES D'ENGIN </a>
+                    </li>
+                    <li>
+                        <a href="#">IMPRESSIONS<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="#">Third Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Third Level Link</a>
+                            </li>
+                            <li>
+                                <a href="#">Third Level Link</a>
+                            </li>
+
+                        </ul> 
+                    </li>  
+
+					                   
+                            
+                  	
+                </ul>
+               
+            </div>
+            
+        </nav>  
+
+
+
+        
+        
+
+
+
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+     <!-- MORRIS CHART SCRIPTS -->
+     <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
+    </div>
+    
+
+
+</x-app-layout>
+
